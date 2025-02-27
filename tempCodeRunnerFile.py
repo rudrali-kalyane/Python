3219a585f68c28a6e52cf462f58ef60a7c483c66
Rudrali kalyane <kalyanerudrali@gmail.com>
2025-02-27 07:48:51 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-#Write a Python program to find the area of a triangle.
-
-base = float(input("Enter the length of base of a triangle : "))
-height = float(input("Enter the height of a triangle : "))
-
-area = 0.5 * height * base
-
-print(f"Area of triangle is {area}")
