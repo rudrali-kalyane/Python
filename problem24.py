c645391c0072cc15f8aa659c901bee369b417ffd
rudrali-kalyane <kalyanerudrali@gmail.com>
2025-03-06 23:30:45 +0530

    day7

---
+++ problem24.py
@@ -0,0 +1,11 @@
+#Write a Python program to find smallest number in a list.
+
+numbers = [30, 10, -45, 5, 20]
+minimum = numbers[0]
+
+for i in numbers:
+    if i < minimum:
+
+        minimum = i
+
+print("The smallest number in the list is:", minimum)
